b157a61209f2ae2bb6d0f23ed18dd88c93ebaa0a
Jonathan Stefanov <38321403+JonathanStefanov@users.noreply.github.com>
2023-12-05 16:17:41 +0100

    Update Sentence_Inference.py

--- pages/Sentence_Inference.py
+++ pages/Sentence_Inference.py
@@ -11,25 +11,17 @@ def main():
     if sentence:
         # Placeholder for model paths (update these as per your model configuration)
         model_path_phase1 = 'phase1.pth'
-<<<<<<< HEAD
         model_path_phase_2_A = 'phase_2_A.pth'
         model_path_phase2_B = 'phase_2_B.pth'
         model_path_phase2_C = 'phase_2_C.pth'
         letter = ''
 
         predictor = Predictor(model_path_phase1=model_path_phase1, model_path_phase2_A=model_path_phase_2_A, model_path_phase2_B=model_path_phase2_B, model_path_phase2_C=model_path_phase2_C)
-=======
-        model_path_phase2 = ''
-        letter = ''
-
-        predictor = Predictor()
->>>>>>> 26c4e849627f7df570f8fcb6f881bfca46d171e1
 
         # Create a DataFrame from the input sentence
         data = pd.DataFrame([sentence], columns=['sentence'])
 
         # Phase 1 Inference
-<<<<<<< HEAD
         predictions_phase1 = predictor.inference_phase(1, data)
 
         # Assuming phase 1 predicts which phase 2 model to use
@@ -42,24 +34,6 @@ def main():
 
         # Phase 2 Inference
         difficulty_level = predictor.inference_phase(2, data, letter)
-=======
-        predictions_phase1 = predictor.inference_phase(1, model_path_phase1, data)
-
-        # Assuming phase 1 predicts which phase 2 model to use
-        if predictions_phase1[0] == 0:
-            model_path_phase2 = 'phase_2_A.pth'
-            letter = 'A'
-        elif predictions_phase1[0] == 1:
-            model_path_phase2 = 'phase_2_B.pth'
-            letter = 'B'
-        else:
-            model_path_phase2 = 'phase_2_C.pth'
-            letter = 'C'
-
-        # Phase 2 Inference
-        difficulty_level = predictor.inference_phase(2, model_path_phase2, data)
->>>>>>> 26c4e849627f7df570f8fcb6f881bfca46d171e1
-
         st.write("Predicted Difficulty Level: ",letter,  str(difficulty_level[0] + 1))
 
 if __name__ == "__main__":
